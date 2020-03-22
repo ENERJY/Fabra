730bb35108b2c56bb0774d4db8b8b139ec15ff07
Dimca <Dimca@DESKTOP-Q1VP5RK>
2020-03-23 00:25:05 +0300

    Commit Staged

--- fabrk/Form1.cs
+++ fabrk/Form1.cs
@@ -41,6 +41,16 @@ namespace fabrk
         {
             this.Close();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
     }
 
