148229ec2ad184613ed270034e30da21ceef0e0c
Dimca <Dimca@DESKTOP-Q1VP5RK>
2020-03-23 00:55:11 +0300

    Commit Staged

--- fabrk/Form1.Designer.cs
+++ fabrk/Form1.Designer.cs
@@ -94,11 +94,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(345, 43);
+            this.label3.Location = new System.Drawing.Point(320, 40);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 24);
+            this.label3.Size = new System.Drawing.Size(129, 24);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Регистрация";
+            this.label3.Text = "Авторизация";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
